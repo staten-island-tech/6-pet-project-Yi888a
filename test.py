e258f9d478d8175c926cf928c097639c6c52ac90
Yi888a <Ziyil8@nycstudents.net>
2025-11-25 09:25:56 -0500

    Updated how the code functions and implemented first version of scrimmages.

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-
+import random
 class Player:
-    def __init__(self, name, overall, height, weight, strength, skills, vertical, on_ball_speed, off_ball_speed, passing, shooting, dribbling, perimeter_defense, interior_defense, driving_dunk, standing_dunk, post_moves, layups, free_throws):
+    def __init__(self, name, height, weight, strength, skills, vertical, on_ball_speed, off_ball_speed, passing, shooting, dribbling, perimeter_defense, interior_defense, driving_dunk, standing_dunk, post_moves, layups, free_throws):
         self.name = name
-        self.overall = (int(skills) + int(shooting) + int(dribbling) + int(perimeter_defense) + int(interior_defense) + int(passing) + int(layups) + int(free_throws)) // 8
         self.height = height
         self.weight = weight
         self.strength = strength
@@ -20,63 +19,74 @@ class Player:
         self.post_moves = post_moves
         self.layups = layups
         self.free_throws = free_throws
-player_position = input("What position do you want to play? ")
-player_name = input("What is your player's name? ")
-if player_position == 'PG':
- player1 = Player(player_name, 0, 190, 180, 60, 70, 70, 70, 75, 80, 75, 75, 70, 55, 50, 35, 60, 75, 75)
-elif player_position == 'SG':
-     player1 = Player(player_name, 0, 195, 185, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80)
-elif player_position == 'SF':
-      player1 = Player(player_name, 0, 198, 190, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80)
-elif player_position == "PF" :
-    player1 = Player(player_name, 0, 205, 230, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80)
-elif player_position == "C":
-     player1 = Player(player_name, 0, 215, 220, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80)
-     
+        self.overall = (skills + shooting + dribbling + perimeter_defense + interior_defense + passing + layups + free_throws) // 8
+        self.pockets = int(0)
 
+def create_player(position, name):
+    stats = {
+        'PG': (190, 180, 60, 70, 70, 70, 75, 80, 75, 75, 70, 55, 50, 35, 60, 75, 75),
+        'SG': (195, 185, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80),
+        'SF': (198, 190, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80),
+        'PF': (205, 230, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80),
+        'C':  (215, 220, 70, 65, 75, 70, 70, 70, 80, 70, 65, 65, 65, 55, 65, 70, 80),
+    }
+    return Player(name, *stats.get(position, (190, 180, 60, 70, 70, 70, 75, 80, 75, 75, 70, 55, 50, 35, 60, 75, 75)))
 
+def display_stats(player):
+    print(f"Player Stats for {player.name}:")
+    for attr, value in vars(player).items():
+        print(f"{attr.capitalize()}: {value}")
 
-print(f"Player Name: {player1.name}, Overall Rating: {player1.overall}")
-print("What would you like to do next? (1) View Player Stats (2) Train Player (3) Scrim (4) Exit")
-choice = input("Enter your choice (1-4): ")
-if choice == '1':
-    print(f"Player Stats for {player1.name}:")
-    print(f"Height: {player1.height} Centimeters")
-    print(f"Weight: {player1.weight} LBs")
-    print(f"Strength: {player1.strength}")
-    print(f"Skills: {player1.skills}")
-    print(f"Vertical: {player1.vertical} inches")
-    print(f"On-Ball Speed: {player1.on_ball_speed}")
-    print(f"Off-Ball Speed: {player1.off_ball_speed}")
-    print(f"Passing: {player1.passing}")
-    print(f"Shooting: {player1.shooting}")
-    print(f"Dribbling: {player1.dribbling}")
-    print(f"Perimeter Defense: {player1.perimeter_defense}")
-    print(f"Interior Defense: {player1.interior_defense}")
-    print(f"Driving Dunk: {player1.driving_dunk}")
-    print(f"Standing Dunk: {player1.standing_dunk}")
-    print(f"Post Moves: {player1.post_moves}")
-    print(f"Layups: {player1.layups}")
-    print(f"Free Throws: {player1.free_throws}")
-elif choice == '2':
-    print("What would you like to do to train your player? (1) Improve Technique (2) Increase Physical Attributes")
-    train_choice = input("Enter your choice (1-2): ")
-if train_choice == '1':
-        player1.shooting += 2
-        player1.dribbling += 2
-        player1.passing += 2
-        print(f"{player1.name}'s technique has improved!")
-elif train_choice == '2':
-        player1.strength += 2
-        player1.vertical += 2
-        player1.on_ball_speed += 2
-        player1.off_ball_speed += 2
-        print(f"{player1.name}'s physical attributes have improved!")
-elif choice == '3':
-    scrim_choice = input("(1) Check scrims avalible (2) Pickup Game")
-if scrim_choice == '1':
-     print ("")
-     
-     
-elif choice == '4':
-     print ("Good Bye")
+def train_player(player):
+    print("Train your player: (1) Improve Technique (2) Increase Physical Attributes")
+    choice = input("Enter your choice (1-2): ")
+    if choice == '1':
+        player.shooting += 2
+        player.dribbling += 2
+        player.passing += 2
+        print(f"{player.name}'s technique has improved!")
+    elif choice == '2':
+        player.strength += 2
+        player.vertical += 2
+        player.on_ball_speed += 2
+        player.off_ball_speed += 2
+        print(f"{player.name}'s physical attributes have improved!")
+    player.overall = (player.skills + player.shooting + player.dribbling + 
+                      player.perimeter_defense + player.interior_defense + 
+                      player.passing + player.layups + player.free_throws) // 8
+
+def scrim(player):
+    print("Scrim in progress...")
+    if player.overall >= 85:
+        print(f"{player.name} performed excellently in the scrim!")
+        print(f"Reward: +{random.randint(50,100)}$ to Pockets")
+    elif player.overall <= 75:
+        print(f"{player.name} got whooped.")
+        train_player(player)
+    else:
+        print(f"Scrim did not go so well...")
+        train_player(player)
+
+def main_menu():
+    position = input("What position do you want to play? (PG/SG/SF/PF/C): ")
+    name = input("What is your player's name? ")
+    player = create_player(position, name)
+    print(f"Player Name: {player.name}, Overall Rating: {player.overall}")
+
+    while True:
+        print("What would you like to do next? (1) View Player Stats (2) Train Player (3) Scrim (4) Exit")
+        choice = input("Enter your choice (1-4): ")
+        if choice == '1':
+            display_stats(player)
+        elif choice == '2':
+            train_player(player)
+        elif choice == '3':
+            scrim(player)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main_menu()
